1c9ea6be7b4d161bb7984a33dd962d4f735223e6
Steven Vergenz <steven.vergenz@microsoft.com>
2019-04-18 15:42:04 -0700

    Stuff

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,8 +1,26 @@
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
-import { log } from '@microsoft/mixed-reality-extension-sdk/built/log';
 
 export class App {
 	constructor(public context: MRE.Context, public baseUrl: string, public params: MRE.ParameterSet = {}) {
+		context.onStarted(() => this.start());
+		context.onStopped(() => this.stop());
+		context.onUserJoined(user => this.userJoin(user));
+		context.onUserLeft(user => this.userLeft(user));
+	}
+
+	public start() {
+
+	}
+
+	public stop() {
+
+	}
+
+	public userJoin(user: MRE.User) {
+
+	}
+
+	public userLeft(user: MRE.User) {
 
 	}
 }
